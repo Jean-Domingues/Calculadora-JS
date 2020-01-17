4478df81c7b488190f5957378e0fee9557d06635
Jean Domingues <jeandomingues752@gmail.com>
2020-01-17 16:06:15 -0300

    Added the files

---
+++ script.js
@@ -0,0 +1,21 @@
+function calcular(tipo, dado) {
+    
+    const visorTela = document.getElementById('visor');
+    if(tipo === 'operador'){
+        if(dado === '-' || dado === '+' || dado === '*' || dado === '/' || dado === '.'){
+            visorTela.value += dado;
+        }else if (dado === 'c'){
+            visorTela.value = '';
+        }
+        else{
+            let resultado = eval(visorTela.value);
+            visorTela.value = resultado;
+            
+        }
+
+    }else{
+        visorTela.value += dado;
+    }
+
+
+}
